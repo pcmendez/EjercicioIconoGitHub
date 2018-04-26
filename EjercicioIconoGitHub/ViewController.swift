a60e0bcc50f606033e0246202b88a48754eb7b7e
Pablo Cruz Medez <pcmendez@icloud.com>
2018-04-26 16:24:37 -0500

    Initial Commit

---
+++ EjercicioIconoGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EjercicioIconoGitHub
+//
+//  Created by Pablo Cruz Medez on 26/04/18.
+//  Copyright © 2018 Pablo Cruz Medez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
